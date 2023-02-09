7701af8a7d7535370f88cc915a85789562e6a054
Santiago Bastidas <santiagobastidasrodriguez@gmail.com>
2023-02-08 19:17:43 -0500

    💡 Login and Auth handle.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-
-import { Product } from './models/product.model';
-
+import { AuthService } from './services/auth/auth.service';
+import { UsersService } from './services/users/users.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   imgParent = '';
   showImg = true;
-
+  /**
+   *
+   */
+  constructor(
+    private _authService: AuthService,
+    private _usersService: UsersService
+  ) {}
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -18,5 +23,27 @@ export class AppComponent {
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this._usersService
+      .create({
+        name: 'Santiago',
+        email: 'santiago@email.com',
+        password: '1702',
+      })
+      .subscribe((rta) => {
+        console.log('====================================');
+        console.log(rta);
+        console.log('====================================');
+      });
+  }
+
+  login() {
+    this._authService.login('santiago@email.com', '1702').subscribe((rta) => {
+      console.log('====================================');
+      console.log(rta.access_token);
+      console.log('====================================');
+    });
   }
 }
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
+import { Auth } from './../../models/auth.model';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = `${environment.API_URL}/api/auth`;
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    return this.http.post<Auth>(`${this.apiUrl}/login`, { email, password });
+  }
+
+  profile() {
+    return this.http.get(`${this.apiUrl}/profile`);
+  }
+}
