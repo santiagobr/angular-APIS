7ee022665b2e676c6859146aa970223a56efa5d4
Santiago Bastidas <santiagobastidasrodriguez@gmail.com>
2023-02-13 11:20:27 -0500

    Create context to intercept specific requests

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpParams,
+  HttpErrorResponse,
+  HttpStatusCode,
+} from '@angular/common/http';
 import { retry, catchError, map } from 'rxjs/operators';
 import { throwError, zip } from 'rxjs';
-
-import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
-
+import {
+  Product,
+  CreateProductDTO,
+  UpdateProductDTO,
+} from './../models/product.model';
 import { environment } from './../../environments/environment';
+import { checkTime } from '../interceptors/time.interceptor';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
   private apiUrl = `${environment.API_URL}/api/products`;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   getAllProducts(limit?: number, offset?: number) {
     let params = new HttpParams();
@@ -24,28 +29,27 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', limit);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params })
-    .pipe(
-      retry(3),
-      map(products => products.map(item => {
-        return {
-          ...item,
-          taxes: .19 * item.price
-        }
-      }))
-    );
+    return this.http
+      .get<Product[]>(this.apiUrl, { params, context: checkTime() })
+      .pipe(
+        retry(3),
+        map((products) =>
+          products.map((item) => {
+            return {
+              ...item,
+              taxes: 0.19 * item.price,
+            };
+          })
+        )
+      );
   }
 
   fetchReadAndUpdate(id: string, dto: UpdateProductDTO) {
-    return zip(
-      this.getProduct(id),
-      this.update(id, dto)
-    );
+    return zip(this.getProduct(id), this.update(id, dto));
   }
 
   getProduct(id: string) {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`)
-    .pipe(
+    return this.http.get<Product>(`${this.apiUrl}/${id}`).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === HttpStatusCode.Conflict) {
           return throwError('Algo esta fallando en el server');
@@ -58,13 +62,13 @@ export class ProductsService {
         }
         return throwError('Ups algo salio mal');
       })
-    )
+    );
   }
 
   getProductsByPage(limit: number, offset: number) {
     return this.http.get<Product[]>(`${this.apiUrl}`, {
-      params: { limit, offset }
-    })
+      params: { limit, offset },
+    });
   }
 
   create(dto: CreateProductDTO) {
